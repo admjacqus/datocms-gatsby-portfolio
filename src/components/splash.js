ac7e7e86c3ea40d019843dc8bcb223f88942d6a5
adm_jacqus <admjacqus@gmail.com>
2020-07-24 11:18:12 +0100

    Splash component

---
+++ src/components/splash.js
@@ -0,0 +1,37 @@
+/* eslint-disable jsx-a11y/anchor-has-content, jsx-a11y/anchor-is-valid*/
+
+import React from "react";
+// import { Link } from "gatsby";
+import { StaticQuery, graphql } from "gatsby";
+
+const TemplateWrapper = () => {
+    return (
+        <StaticQuery
+            query={graphql`
+       query SplashQuery {
+
+          datoCmsHome {
+        copyright
+          }
+        }
+      `}
+            render={data => (
+                <div className="splash">
+
+                <div className="splash__content">
+                        <h1 className="xxl">Hi, I'm Adam Jacques.</h1>
+
+                    <div className="splash__copyright">
+                        {data.datoCmsHome.copyright}
+                    </div>
+                    </div>
+                
+                </div>
+            )}
+        />
+    );
+};
+
+
+export default TemplateWrapper;
+/* eslint-enable jsx-a11y/anchor-has-content, jsx-a11y/anchor-is-valid*/
